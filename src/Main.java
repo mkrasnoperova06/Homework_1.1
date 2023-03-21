c45aeb6fea9219a9a8368ce68f6bb8df7867842c
Marina <mkrasnoperova06@yandex.ru>
2023-03-21 22:06:52 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 7;
+
+        boolean ggg = false;
+
+        if (x > 1)
+        {
+            ggg = true;
+        }
+        else
+        {
+            x = 0;
+        }
+
+        System.out.print(ggg);
+    }
+}
